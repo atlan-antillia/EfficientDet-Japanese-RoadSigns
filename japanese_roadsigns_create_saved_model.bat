33cde0097f72bf48927597f55c0e849e582de67c
atlan-antillia <toshiyuki.arai.uz@gmail.com>
2021-08-25 15:29:51 +0900

    First commit

---
+++ japanese_roadsigns_create_saved_model.bat
@@ -0,0 +1,1 @@
+python model_inspect.py --runmode=saved_model --model_name=efficientdet-d0 --ckpt_path=./projects/Japanese_RoadSigns/models  --hparams="image_size=512x512" --saved_model_dir=./projects/Japanese_RoadSigns/saved_model
---
+++ japanese_roadsigns_detect.bat
@@ -0,0 +1,1 @@
+python model_inspect.py --runmode=saved_model_infer --model_name=efficientdet-d0 --saved_model_dir=./projects/Japanese_RoadSigns/saved_model --min_score_thresh=0.3 --hparams=./projects/Japanese_RoadSigns/configs/default.yaml --input_image=./projects/Japanese_RoadSigns/test/*.jpg --output_image_dir=./projects/Japanese_RoadSigns/outputs
